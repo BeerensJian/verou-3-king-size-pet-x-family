03455ed21e640305becc283d15bb42fa3bdfa6b2
JianB <Beerensjian@gmail.com>
2022-05-23 12:22:34 +0200

    Seperating routes

--- server.mjs
+++ server.mjs
@@ -1,11 +1,20 @@
 import express from "express";
+import mainRouter from "./routes/main.mjs"
 const app = express();
+
 app.set('view engine', 'ejs')
 app.use(express.static("dist"));
 
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+// app.get("/", (req, res) => {
+//     res.render("index");
+// })
+// app.get("/login", (req, res) => {
+//     res.render("login");
+// })
+// app.get("/signup", (req, res) => {
+//     res.render("signup");
+// })
 
+app.use("/", mainRouter)
 app.listen(3000);
