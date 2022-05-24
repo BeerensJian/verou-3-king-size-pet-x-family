993638ab71e3ff165062f743b7b02f2a10fc8ecc
JianB <Beerensjian@gmail.com>
2022-05-24 15:42:58 +0200

    Small changes

--- routes/main.mjs
+++ routes/main.mjs
@@ -2,7 +2,13 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index")
+    res.render("index");
+})
+router.get("/login", (req, res) => {
+    res.render("login");
+})
+router.get("/signup", (req, res) => {
+    res.render("signup");
 })
 
 export default router;
--- server.mjs
+++ server.mjs
@@ -1,20 +1,10 @@
 import express from "express";
-import mainRouter from "./routes/main.mjs"
+import mainRouter from "./routes/main.mjs";
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.use(express.static("dist"));
 
-
-// app.get("/", (req, res) => {
-//     res.render("index");
-// })
-// app.get("/login", (req, res) => {
-//     res.render("login");
-// })
-// app.get("/signup", (req, res) => {
-//     res.render("signup");
-// })
-
-app.use("/", mainRouter)
+/* Routes */
+app.use("/", mainRouter);
 app.listen(3000);
