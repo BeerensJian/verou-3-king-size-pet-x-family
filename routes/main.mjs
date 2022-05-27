15cd2d86c355d1aeeed335ef8af871ee965d6ff2
Sa-raCha <sarabibimagdalena@gmail.com>
2022-05-27 18:04:25 +0200

    Adding dashboard placeholder content and styling

--- routes/main.mjs
+++ routes/main.mjs
@@ -10,6 +10,8 @@ router.get("/login", (req, res) => {
 router.get("/signup", (req, res) => {
     res.render("signup");
 })
-
+router.get("/dashboard", (req, res) => {
+    res.render("dashboard");
+})
 
 export default router;
