12d8455f5462f6188f192009575019599969ff93
JianB <Beerensjian@gmail.com>
2022-05-30 07:28:30 +0200

    Added mongoDB connection

---
+++ config.mjs
@@ -0,0 +1,3 @@
+const mongoURI = "mongodb+srv://admin:admin@cluster0.j6lwd.mongodb.net/";
+
+export default mongoURI;
--- server.mjs
+++ server.mjs
@@ -1,9 +1,18 @@
 import express from "express";
 import mainRouter from "./routes/main.mjs";
+import mongoose from "mongoose";
+import mongoURI from "./config.mjs"
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static("dist"));
+
+mongoose.connect(mongoURI + "sessions" , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(res => {
+    console.log("Mongo connected")
+})
 
 /* Routes */
 app.use("/", mainRouter);
