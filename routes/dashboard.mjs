ad3148cf68ffba597953b5fcb7d4e2f59cb20b10
JianB <Beerensjian@gmail.com>
2022-06-05 11:22:28 +0200

    Delete pet & confirmation

--- routes/dashboard.mjs
+++ routes/dashboard.mjs
@@ -6,12 +6,13 @@ import UserModel from "../Models/User.mjs"
 import getGenderIcon from '../helpers/getGenderIcon.mjs';
 import flash from 'connect-flash';
 
+
 const router = express.Router()
 
 router.get("/", isAuth, async (req, res) => {
 
     const pets = await PetModel.find({ownerID: req.session.ownerID})
-    res.render("dashboard", {pets: pets, getGenderIcon : getGenderIcon, showAge : showAge});
+    res.render("dashboard", {pets: pets, getGenderIcon : getGenderIcon, showAge : showAge, msg : req.flash("msg")});
 })
 router.get("/addpet", isAuth, (req, res) => {
     res.render("addpet", {err : req.flash("msg")});
@@ -19,7 +20,7 @@ router.get("/addpet", isAuth, (req, res) => {
 router.post("/addpet", isAuth, async (req, res) => {
     const {name, gender, birthday, neutered, microshipNr, breed, typeOfAnimal, status} = req.body;
 
-    if (name == "" || gender == "" || birthday == "" || !neutered || breed == "" || typeOfAnimal == "") {
+    if (name == "" || gender == "" || birthday == "" || !neutered  || typeOfAnimal == "") {
         req.flash("msg", "Please fill in all the fields");
         return res.redirect("/dashboard/addpet");
     }
@@ -30,7 +31,7 @@ router.post("/addpet", isAuth, async (req, res) => {
         birthday,
         neutered,
         microshipNr,
-        breed,
+        breed : breed || "None Specified",
         typeOfAnimal,
         status,
         ownerID: req.session.ownerID
@@ -47,7 +48,28 @@ router.get("/pet/:id", async (req, res) => {
     const pet = await PetModel.findOne({ _id : req.params.id})
     const owner = await UserModel.find({ _id : pet.ownerID})
 
-    res.render("pet")
+    res.render("pet", {pet: pet, owner : owner, showDate: showDate,})
+})
+
+router.post("/delete/:id", async (req, res) => {
+    
+    let pet = await PetModel.findOneAndRemove({ _id : req.params.id}).where({ ownerID : req.session.ownerID })
+    console.log(pet)
+    if ( pet == null ) {
+        req.flash("msg", "No pet found or you're not the owner")
+        return res.redirect("/dashboard")
+    }
+    req.flash("msg", "Pet deleted succesfully!")
+    res.redirect("/dashboard")
+
+    // if (req.session.ownerID == pet.ownerID) {
+    //     pet = await PetModel.deleteOne({ id : req.params.id})
+    //     req.flash("confirm", "Pet succesfully deleted")
+    //     res.redirect("/dashboard")
+    // } else {
+    //     res.redirect("/dashboard/pet/" + pet.id)
+    // }
+    
 })
 
 
