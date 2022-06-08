8b7215b05e62341f78b0d515c709aae7abb8f937
JianB <Beerensjian@gmail.com>
2022-06-08 12:19:47 +0200

    Makign project heroku ready

--- database/db.mjs
+++ database/db.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import mongoURI from "../config.js"
 
 const connectMongoDB = () => {
-    mongoose.connect(mongoURI + "sessions" , {
+    mongoose.connect("mongodb+srv://admin:admin@cluster0.j6lwd.mongodb.net/" + "sessions" , {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(res => {
